ada8338fa9879754c9515e4a6d69c7499ebc8474
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-08-05 15:39:01 +0100

    about page

---
+++ src/routes/about/+page.server.ts
@@ -0,0 +1,35 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+const getBioFromDatabase = async () => {
+	const API_URL = 'https://gregemyers-api-fly.fly.dev/api/bio';
+	const BEARER_TOKEN = import.meta.env.VITE_BEARER_TOKEN;
+
+	const response = await fetch(API_URL, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${BEARER_TOKEN}`
+		}
+	});
+
+	if (!response.ok) {
+		throw new Error(`Failed to fetch bio data: ${response.statusText}`);
+	}
+
+	const data = await response.json();
+	return data.data.attributes.text;
+};
+
+export const load: PageServerLoad = async () => {
+	try {
+		const bio = await getBioFromDatabase();
+
+		return {
+			props: {
+				bio // This assumes your template expects a `bio` prop
+			}
+		};
+	} catch (e) {
+		throw error(404, 'Not found');
+	}
+};
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>About this site</h1>
+<p>{@html data.props.bio}</p>
+<a href="/">Home</a>
