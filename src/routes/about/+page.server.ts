942a283b9aad9e662977027d0a81cdc3c0ca29f2
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-08-21 21:31:05 +0100

    basic contact page

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -28,7 +28,7 @@ export const load: PageServerLoad = async () => {
 
 		return {
 			props: {
-				bio // This assumes your template expects a `bio` prop
+				bio
 			}
 		};
 	} catch (e) {
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<main>
+	<h2>Contact</h2>
+	<p>Please enquire for sales, commissions and gallery shows.</p>
+	Email: contact@gregemyers.com<br />
+	Instagram:{' '}
+	<a href="https://www.instagram.com/gregemyers" target="_blank"> @gregemyers </a>
+</main>
+
+<style>
+</style>
