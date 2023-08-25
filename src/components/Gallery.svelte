8d1dd76178f52d68e57e46b6684ed223181619e9
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-08-25 19:04:17 +0100

    revert

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
 	import { fetchGalleryImages } from '$lib/apiService';
 	import type { GalleryImage } from '../types';
-	import { onMount } from 'svelte';
 
 	let galleryImages: GalleryImage[] = [];
 
-	onMount(async () => {
+	async function loadGalleryImages() {
 		galleryImages = await fetchGalleryImages();
-	});
+	}
+
+	loadGalleryImages();
 </script>
 
 <div>
-	{#if galleryImages.length > 0}
-		{#each galleryImages as image}
-			<div class="image-item">
-				<img src={image.url} alt={image.alt} />
-				<h3>{image.title}</h3>
-				<p>{image.caption}</p>
-			</div>
-		{/each}
-	{:else}
-		<p>Loading gallery images...</p>
-	{/if}
+	{#each galleryImages as image}
+		<div class="image-item">
+			<img src={image.url} alt={image.alt} />
+			<h3>{image.title}</h3>
+			<p>{image.caption}</p>
+		</div>
+	{/each}
 </div>
